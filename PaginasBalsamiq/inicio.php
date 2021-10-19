17ac779ed8ccc68101dd834036f5eb4101570f08
Nasher3296 <ignaciofernandez3296@gmail.com>
2021-10-19 09:48:09 -0300

    amongas

--- PaginasBalsamiq/inicio.php
+++ PaginasBalsamiq/inicio.php
@@ -24,7 +24,7 @@
         $_SESSION['imagen'] = $usuarioSession['imagen'];
     ?>
 
-<header class="header">
+    <header class="header">
         <h1 class="ISolveIt">I solve it</h1>
         <input class="buscador" type="text" placeholder="buscar...">
     </header>
@@ -48,66 +48,23 @@
     <div class="main">
         <div class="publicaciones">
         <?php
-                $consulta = $conn->prepare("SELECT * FROM consulta WHERE id_consulta = 6"); 
-                $consulta ->execute();
-                $resultadoCon = $consulta->fetch(PDO::FETCH_ASSOC);
-
-                $consulta = $conn->prepare("SELECT * FROM usuario WHERE id_us= '".$resultadoCon['id_us']."'"); 
-                $consulta ->execute();
-        
-                $resultadoUser = $consulta->fetch(PDO::FETCH_ASSOC);
-                
-                
-
-                echo'
-                <div class="publicacion_preview">
-                <div class="data">
-                    <div class="foto">
-                        A
-                        <!--<img class="fotoPerfil" src="recursos/fotoPerfil/'.$resultadoUser['imagen'].'.png" alt="'.$resultadoUser['username'].'">-->
-                    </div>
-                    <div class="usuario">
-                        <h4>'.$resultadoUser["nombre"].'</h4>
-                        <h5>@'.$resultadoUser["username"].'</h5>
-                    </div>
-                    <div class="recoyvenc">
-                        <h4>Recompensa: $'.$resultadoCon["recompensa"].'</h4>
-                        <!--Para el vencimiento un simbolito de reloj y el tiempo restante-->
-                        <h4>Vencimiento: '.$resultadoCon["fecha_limite"].'</h4>
-                    </div>
-                </div>
-                <div class="cuerpo">
-                    <h2>'.$resultadoCon['titulo'].'</h2>
-                    <!--Descripcion-->
-                    <p>'.$resultadoCon['descripcion'].'</p>
-                </div>
-                <div class="tags">
-                    <!--Que aparezcan iconos de archivos en caso de haberlos, similar a gmail-->
-                    <h4>Etiquetas: </h4>
-                    <ul class="tags_list">
-                ';
-                if($resultadoCon['tag']){
-                    $tags = explode(",", $resultadoCon['tag']);
-                    foreach($tags as $t){
-                        echo'<li class="tag">'.$t.'</li>';
-                    }
-                }
-                            
-                echo'
-                            </ul>
-                        </div>
-                    </div>
-                ';
-
-
+            
 
                 $consulta = $conn->prepare("SELECT tag_us FROM tag_usuario WHERE id_us = '".$_SESSION['id_us']."'");
                 $consulta ->execute();
                 while($resultadoTagUsr = $consulta->fetch(PDO::FETCH_ASSOC)){
-                    $consulta2 = $conn2->prepare("SELECT tag FROM tag WHERE id = '".$resultadoTagusr['tag_us']."'");
+                    $consulta2 = $conn->prepare("SELECT id_cons FROM tag_cons WHERE tag_cons = '".$resultadoTagUsr['tag_us']."'");
                     $consulta2 ->execute();
-                    while($resultadoTag = $consulta2->fetch(PDO::FETCH_ASSOC)){
-                    }
+                    $resultadoTag = $consulta2->fetch(PDO::FETCH_ASSOC);
+
+                    $consulta3 = $conn->prepare("SELECT * FROM consulta WHERE id_consulta = '".$resultadoTag['id_cons']."'");
+                    $consulta3 ->execute();
+                    $resultadoTag = $consulta3->fetch(PDO::FETCH_ASSOC);
+
+                    $consulta4 = $conn->prepare("SELECT * FROM usuario WHERE id_us = '".$resultadoTag['id_us']."'");
+                    $consulta4 ->execute();
+                    $resultadoUser = $consulta4->fetch(PDO::FETCH_ASSOC);
+                    
 
 
                     echo'
@@ -122,29 +79,20 @@
                                 <h5>@'.$resultadoUser["username"].'</h5>
                             </div>
                             <div class="recoyvenc">
-                                <h4>Recompensa: $'.$resultadoCon["recompensa"].'</h4>
+                                <h4>Recompensa: $'.$resultadoTag["recompensa"].'</h4>
                                 <!--Para el vencimiento un simbolito de reloj y el tiempo restante-->
-                                <h4>Vencimiento: '.$resultadoCon["fecha_limite"].'</h4>
+                                <h4>Vencimiento: '.$resultadoTag["fecha_limite"].'</h4>
                             </div>
                         </div>
                         <div class="cuerpo">
-                            <h2>'.$resultadoCon['titulo'].'</h2>
+                            <h2>'.$resultadoTag['titulo'].'</h2>
                             <!--Descripcion-->
-                            <p>'.$resultadoCon['descripcion'].'</p>
+                            <p>'.$resultadoTag['descripcion'].'</p>
                         </div>
                         <div class="tags">
                             <!--Que aparezcan iconos de archivos en caso de haberlos, similar a gmail-->
                             <h4>Etiquetas: </h4>
                             <ul class="tags_list">
-                        ';
-                        if($resultadoCon['tag']){
-                            $tags = explode(",", $resultadoCon['tag']);
-                            foreach($tags as $t){
-                                echo'<li class="tag">'.$t.'</li>';
-                            }
-                        }
-                                    
-                        echo'
                                     </ul>
                                 </div>
                             </div>
