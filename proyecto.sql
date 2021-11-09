69c28e4d0ee8ed4bd7b097b4653d2699adc52d50
Nasher3296 <ignaciofernandez3296@gmail.com>
2021-11-09 07:09:47 -0300

    amogus

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-11-2021 a las 00:30:50
+-- Tiempo de generación: 09-11-2021 a las 11:09:11
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.10
 
@@ -38,7 +38,8 @@ CREATE TABLE `asignado` (
 --
 
 INSERT INTO `asignado` (`id`, `id_consulta`, `id_us`) VALUES
-(21, 15, 28);
+(21, 15, 28),
+(25, 17, 32);
 
 -- --------------------------------------------------------
 
@@ -58,7 +59,8 @@ CREATE TABLE `concurso` (
 
 INSERT INTO `concurso` (`id_concurso`, `id_consulta`, `id_us`) VALUES
 (100, 11, 27),
-(102, 12, 28);
+(102, 12, 28),
+(109, 16, 26);
 
 -- --------------------------------------------------------
 
@@ -86,7 +88,8 @@ INSERT INTO `consulta` (`id_consulta`, `id_us`, `titulo`, `descripcion`, `recomp
 (12, 26, 'Polinomios de segundo grado', 'Necesito ayuda con este trabajo para el colegio. No entiendo el tema', 100, '2021-10-31 19:48:06', '2021-11-05 19:45:00', '../archivos/Trabajo sobre polinomios.pdf'),
 (13, 27, 'economia en tiempos de Jesus', 'Elaborar un informe sobre las diferencias sociales rondando el año 0 d.c.', 400, '2021-10-31 19:53:02', '2021-11-06 19:51:00', '../archivos/Economia en tiempos de Jesus.pdf'),
 (15, 27, 'Problema de DNS', 'Al intentar entrar en cuevana me aparece este error (adjunto imagen) en pantalla. Ayuda! quiero ver doctor milagro', 50, '2021-10-31 19:59:57', '2021-11-04 19:59:00', '../archivos/Fix-Server-DNS-address-could-not-be-found-error-in-Chrome.png'),
-(16, 28, 'coreccion de hortografia', 'alluda nesesito que alguien haga una rebision a este trabajo', 225, '2021-10-31 20:08:21', '2021-11-25 20:06:00', '../archivos/El principito.pdf');
+(16, 28, 'coreccion de hortografia', 'alluda nesesito que alguien haga una rebision a este trabajo', 225, '2021-10-31 20:08:21', '2021-11-25 20:06:00', '../archivos/El principito.pdf'),
+(17, 30, 'Ayuda con programacion', 'Necesito una mano con este trabajo pracito. No pude prestar mucha atencion a la clase', 250, '2021-11-02 22:15:59', '2021-11-18 00:00:00', '../archivos/Act Nro 7- Ej. Final de Vectores y Funciones.docx');
 
 -- --------------------------------------------------------
 
@@ -107,7 +110,8 @@ CREATE TABLE `entregado` (
 --
 
 INSERT INTO `entregado` (`id`, `id_cons`, `id_us`, `descripcion`, `archivo`) VALUES
-(44, 13, 26, 'Ahi está subido, mucha suerte!', '../archivos/La economia en tiempos de Jesus.pdf');
+(44, 13, 26, 'Ahi está subido, mucha suerte!', '../archivos/La economia en tiempos de Jesus.pdf'),
+(45, 17, 32, 'Aca está la resolucion. Espero que te sirva!', '../archivos/Resolucion.docx');
 
 -- --------------------------------------------------------
 
@@ -140,7 +144,8 @@ INSERT INTO `tag` (`id`, `tag`) VALUES
 (19, 'economia'),
 (20, 'google'),
 (21, 'dns'),
-(22, 'chrome');
+(22, 'chrome'),
+(23, 'funciones');
 
 -- --------------------------------------------------------
 
@@ -171,7 +176,9 @@ INSERT INTO `tag_cons` (`id`, `id_cons`, `tag_cons`) VALUES
 (23, 15, 20),
 (24, 15, 21),
 (25, 15, 22),
-(26, 16, 2);
+(26, 16, 2),
+(27, 17, 11),
+(28, 17, 23);
 
 -- --------------------------------------------------------
 
@@ -197,7 +204,13 @@ INSERT INTO `tag_usuario` (`id`, `id_us`, `tag_us`) VALUES
 (50, 28, 21),
 (51, 26, 2),
 (52, 26, 13),
-(53, 26, 21);
+(53, 26, 21),
+(54, 29, 1),
+(55, 30, 1),
+(57, 31, 1),
+(58, 31, 11),
+(60, 32, 1),
+(61, 32, 11);
 
 -- --------------------------------------------------------
 
@@ -226,7 +239,11 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`id_us`, `username`, `nombre`, `pass`, `mail`, `estrellas`, `empleo`, `descripcion`, `tag`, `tokens`, `imagen`) VALUES
 (26, 'juanperez', 'El Jaime 3296', '$2y$10$zSw3EkB/LM/fDLDTy1YWUeNFd9.iETnCTENLbPQIyPFRHpu.AGAyS', 'juanperez', NULL, 0, '\"Hola. Soy un nuevo usuario de I Solve It. Espero que podamos ayudarnos mutuamente!', 'matematica,historia,literatura', 500, 0),
 (27, 'GonzaPuga', 'Gonzaa', '$2y$10$Qj/1UgyQxeRTmH5QjP28/u.CHaSB74qtTR/EvbW3Hc0QCSgnJYL46', 'GonzaPuga@hotmail.com', NULL, 0, '\"Hola. Soy un nuevo usuario de I Solve It. Espero que podamos ayudarnos mutuamente!', 'matematica,historia,literatura', 46, 0),
-(28, 'Amonger', 'Nuevo usuario', '$2y$10$rkqkSkfbnVkkdjcMXYZdA.ukMs.lGhUDGOlULBDC/BbNB0vxNBDEi', 'amongus@impostor.com', NULL, 0, '\"Hola. Soy un nuevo usuario de I Solve It. Espero que podamos ayudarnos mutuamente!', 'matematica,historia,literatura', 275, 0);
+(28, 'Amonger', 'Nuevo usuario', '$2y$10$rkqkSkfbnVkkdjcMXYZdA.ukMs.lGhUDGOlULBDC/BbNB0vxNBDEi', 'amongus@impostor.com', NULL, 0, '\"Hola. Soy un nuevo usuario de I Solve It. Espero que podamos ayudarnos mutuamente!', 'matematica,historia,literatura', 275, 0),
+(29, 'Consultor', 'El Consultor', '$2y$10$t9NFHWCVmoHlB5728.0eJ.KiOn8OIHNt0lBR9v3FKRixMGkQsMqka', 'Consultor@gmail.com', NULL, 0, '\"Hola. Soy un nuevo usuario de I Solve It. Espero que podamos ayudarnos mutuamente!', 'matematica,historia,literatura', 500, 0),
+(30, 'RodriPregunta', 'Rodrigo Pascual', '$2y$10$gy38HpI4ykUrOL7Npsr9/.w8qQ8XmGw9Z51q..yyF/e5u2GYGfvqe', 'RodriPregunta@gmail.com', NULL, 0, '\"Hola. Soy un nuevo usuario de I Solve It. Espero que podamos ayudarnos mutuamente!', 'matematica,historia,literatura', 250, 0),
+(31, 'PepeResponde', 'Pepe Escobar', '$2y$10$lMuGtuPJoA5WyNQIkP2pbOPqHz739LBLmvefDNqV3j8uJd29FcjMO', 'PepeResponde@gmail.com', NULL, 0, '\"Hola. Soy un nuevo usuario de I Solve It. Espero que podamos ayudarnos mutuamente!', 'matematica,historia,literatura', 500, 0),
+(32, 'MarcoTulio', 'Marquiño Tulinho', '$2y$10$FnX7l0SYGLCDLhsbZtDTweReXeEbfT210VgQfF5XHDJhz0WHz9pd.', 'MarcoTulio@gmail.com', NULL, 0, '\"Hola. Soy un nuevo usuario de I Solve It. Espero que podamos ayudarnos mutuamente!', 'matematica,historia,literatura', 750, 0);
 
 --
 -- Índices para tablas volcadas
@@ -289,49 +306,49 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `asignado`
 --
 ALTER TABLE `asignado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `concurso`
 --
 ALTER TABLE `concurso`
-  MODIFY `id_concurso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `id_concurso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
 
 --
 -- AUTO_INCREMENT de la tabla `consulta`
 --
 ALTER TABLE `consulta`
-  MODIFY `id_consulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_consulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `entregado`
 --
 ALTER TABLE `entregado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT de la tabla `tag`
 --
 ALTER TABLE `tag`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `tag_cons`
 --
 ALTER TABLE `tag_cons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `tag_usuario`
 --
 ALTER TABLE `tag_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_us` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id_us` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- Restricciones para tablas volcadas
